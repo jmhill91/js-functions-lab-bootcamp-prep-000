c5e1d3afd4dfea52f31ef48092ff1d48c70d5265
John Hill <john.m.hill91@gmail.com>
2019-01-10 01:17:13 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ function happyHolidays(string){
   return "Happy holidays!"
 }
 function happyHolidaysTo(name){
-  return `Happy holidays, ${name}`
+  return `Happy holidays, ${name}!`
 }
 function happyHolidaysTo(holiday, name){
   return `Happy ${holiday}, ${name}!`
